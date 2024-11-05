f5254f19035aaeac20b819a6bfa7f1bb0d92dbd0
Adam Snyder <adam555snyder@gmail.com>
2024-11-05 16:17:30 -0600

    edits to SyncToCameraXRotation script and Aim Target Origin

--- Assets/Scripts/SyncToCameraXRotation.cs
+++ Assets/Scripts/SyncToCameraXRotation.cs
@@ -33,18 +33,15 @@ public class SyncToCameraXRotation : MonoBehaviour
 
         if (camTransform == null || aimTarget == null) return;
 
-        if (PV.IsMine)
-        {
-            // Sync Y rotation of Aim Target Origin with camTransform
-            Vector3 originRotation = transform.localRotation.eulerAngles;
-            originRotation.y = camTransform.localRotation.eulerAngles.y;
-            transform.localRotation = Quaternion.Euler(originRotation);
+        // Sync Y rotation of Aim Target Origin with camTransform
+        Vector3 originRotation = transform.localRotation.eulerAngles;
+        originRotation.y = camTransform.localRotation.eulerAngles.y;
+        transform.localRotation = Quaternion.Euler(originRotation);
 
-            // Sync X rotation of aimTarget with camTransform
-            Vector3 aimTargetRotation = aimTarget.localRotation.eulerAngles;
-            aimTargetRotation.x = camTransform.localRotation.eulerAngles.x;
-            aimTarget.localRotation = Quaternion.Euler(aimTargetRotation);
-        }
-        
+        // Sync X rotation of aimTarget with camTransform
+        Vector3 aimTargetRotation = aimTarget.localRotation.eulerAngles;
+        aimTargetRotation.x = camTransform.localRotation.eulerAngles.x;
+        aimTarget.localRotation = Quaternion.Euler(aimTargetRotation);
+
     }
 }
