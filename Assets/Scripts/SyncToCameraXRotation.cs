39504b3807f7df8195af5938cedbf43a69645622
Adam Snyder <adam555snyder@gmail.com>
2024-11-05 15:00:01 -0600

    edits to SyncToCameraXRotation script and Aim Target Origin

--- Assets/Scripts/SyncToCameraXRotation.cs
+++ Assets/Scripts/SyncToCameraXRotation.cs
@@ -7,43 +7,38 @@ public class SyncToCameraXRotation : MonoBehaviour
 {
     [SerializeField] Transform camTransform;
     [SerializeField] Transform aimTarget;
-    PhotonView PV;
+    //PhotonView PV;
 
     private void Awake()
     {
-        PV = transform.parent.parent.GetComponent<PhotonView>();
-
-        if (PV.IsMine)
-        {
-            //camTransform = transform.parent.transform.Find("Camera Offset").Find("Main Camera").transform;
-            //camTransform = transform.parent.transform.Find("Tracking").transform;
-            camTransform = transform.parent;
-        }
+        //PV = transform.parent.parent.GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        /*
         if (PV.IsMine)
         {
-            /*
+            
             // Sync the Y rotation of the model with the camera's Y rotation
             Vector3 currentLocalRotation = transform.localRotation.eulerAngles;
             currentLocalRotation.x = camTransform.localRotation.eulerAngles.x;
             //currentRotation.z = camTransform.rotation.eulerAngles.z;
             transform.localRotation = Quaternion.Euler(currentLocalRotation);
-            */
+            
 
-            // Sync Y rotation with Tracking object for Aim Target Origin
-            Vector3 originRotation = transform.localRotation.eulerAngles;
-            originRotation.y = camTransform.localRotation.eulerAngles.y;
-            transform.localRotation = Quaternion.Euler(originRotation);
 
-            // Sync X rotation for Aim Target itself
-            Vector3 aimTargetRotation = aimTarget.localRotation.eulerAngles;
-            aimTargetRotation.x = camTransform.localRotation.eulerAngles.x;
-            aimTarget.localRotation = Quaternion.Euler(aimTargetRotation);
-        }
+        }*/
 
+        // Sync Y rotation with Tracking object for Aim Target Origin
+        Vector3 originRotation = transform.localRotation.eulerAngles;
+        originRotation.y = camTransform.localRotation.eulerAngles.y;
+        transform.localRotation = Quaternion.Euler(originRotation);
+
+        // Sync X rotation for Aim Target itself
+        Vector3 aimTargetRotation = aimTarget.localRotation.eulerAngles;
+        aimTargetRotation.x = camTransform.localRotation.eulerAngles.x;
+        aimTarget.localRotation = Quaternion.Euler(aimTargetRotation);
     }
 }
